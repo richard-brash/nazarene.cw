1efe28d6ccf042d2650ea30071c6d40b8b8c685d
richardbrash <richard.brash@richardbrashmarketing.com>
2015-07-30 16:10:08 -0400

    Add Contact us form

--- routes/signup.js
+++ routes/signup.js
@@ -3,7 +3,6 @@
  */
 
 var express = require('express');
-var InfusionsoftApiClient = require('../lib/InfusionsoftApiClient');
 
 var router = express.Router();
 
@@ -11,135 +10,42 @@ router.get("/", function(req,res){
     res.json({Message:"Here we are"});
 });
 
-router.post("/addToGroup", function(req,res){
-
-    var input = req.body;
-    InfusionsoftApiClient.Caller("ContactService.addToGroup", [input.cid, parseInt(input.grpId)], function(error, valuegrp){
-        res.json(valuegrp);
-    });
-
-});
-
 router.post("/formhandler", function(req,res){
 
     var input = req.body;
 
-    var grpid = null;
-    var asid = null;
-    var successUrl = null;
-    var errorUrl = null;
-    var clientid = null;
-    var dupcheck = null;
-    var groups = [];
-    var posted = {};
+    //var sendgrid = require('sendgrid')("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", "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");
+    var sendgrid = require('sendgrid')("richardbrash", "rbmP@ssw0rd");
+    var email = new sendgrid.Email();
 
-    for(var attrib in input){
+    email.addTo("richard@richardbrash.com");
+    email.addTo("jonathan.petrus@gmail.com");
+    email.setFrom(input.Email);
+    email.setSubject("Someone sent a note from nazarenecuracao.org");
 
-        switch(attrib){
-            case "clientid":
-                clientid = input[attrib];
-                break;
-            case "dupcheck":
-                dupcheck = input[attrib];
-                break;
-            case "successUrl":
-                successUrl = input[attrib];
-                break;
-            case "errorUrl":
-                errorUrl = input[attrib];
-                break;
-            case "runaction":
-                asid = input[attrib];
-                break;
-            case "addtogroup":
-                grpid = input[attrib];
-                break;
-            case "groups":
-                groups = input[attrib];
-            default:
-                posted[attrib] = input[attrib];
+    email.setText(
+        "First Name:" + input.FirstName + "\n" +
+        "LastName Name:" + input.LastName + "\n" +
+        "Email:" + input.Email + "\n" +
+        "Note:" + input.Note + "\n"
+    );
 
-        }
+    email.setHtml(
+        "First Name:<b>" + input.FirstName + "</b><br/>" +
+        "Last Name:<b>" + input.LastName + "</b><br/>" +
+        "Email:<b>" + input.Email + "</b><br/>" +
+        "Note:<b>" + input.Note + "</b><br/>"
+    );
 
-    }
+    sendgrid.send(email, function(error, result){
 
-    var email = posted["Email"];
-    var service = null;
-    var packet = [posted];
-
-    if(dupcheck == null){
-        service = "ContactService.add";
-    } else{
-        service = "ContactService.addWithDupCheck";
-        packet = packet.concat(dupcheck);
-
-    }
-
-    InfusionsoftApiClient.Caller(service, packet, function(error, value){
-
-        if(!error){
-
-            if(email){
-                InfusionsoftApiClient.Caller("APIEmailService.optIn", [email, "Team member signup"], function(error, optin){
-
-                    if(error){
-                        console.log("Optin error:" + error);
-                    } else {
-                        console.log("Optin:" + optin);
-                    }
-
-                });
-            }
-
-            if(grpid != null){
-                InfusionsoftApiClient.Caller("ContactService.addToGroup", [value, parseInt(grpid)], function(error, valuegrp){
-                    if(error){
-                        console.log("addToGroup error:" + error);
-                    } else {
-                        console.log("addToGroup:" + valuegrp);
-                    }
-
-                });
-            }
-
-            if(asid != null){
-                InfusionsoftApiClient.Caller("ContactService.runActionSequence", [value, parseInt(asid)], function(error, valueas){
-
-                    if(error){
-                        console.log("runActionSequence error:" + error);
-                    } else {
-                        console.log("runActionSequence:" + valueas);
-                    }
-
-                });
-            }
-
-            if(groups.length != 0){
-                for (var gid in groups) {
-                    InfusionsoftApiClient.Caller("ContactService.addToGroup", [value, parseInt(groups[gid])], function(error, valuegrp){
-                        if(error){
-                            console.log("addToGroup error:" + error);
-                        } else {
-                            console.log("addToGroup:" + groups[gid] + " " + valuegrp);
-                        }
-
-
-                    });
-                }
-
-            }
-
-
-            posted["Id"] = value;
-            posted["message"] = "Data posted";
-
-            res.json(posted);
-
-
-        } else {
+        if(error){
             res.json(error);
+        }else {
+            res.json(result);
         }
     });
+
 
 });
 
